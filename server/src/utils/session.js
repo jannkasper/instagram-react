824f6348f4e3cb828ea31c66f68f66593a32cf5a
jannkasper <jann.kasper@gmail.com>
2021-02-15 07:28:59 +0100

    heroku | puppeteer fix

--- server/src/utils/session.js
+++ server/src/utils/session.js
@@ -14,6 +14,7 @@ async function startBrowser() {
     const browser = await puppeteer.launch({
         headless: true,
         args: [
+            '--no-sandbox',
             '--window-size=1920,1080',
             '--user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"'
         ]

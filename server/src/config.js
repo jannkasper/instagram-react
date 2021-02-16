d388789e4cf9c073b795af71359ac2cefc7e7c21
jannkasper <jann.kasper@gmail.com>
2021-02-16 07:10:52 +0100

    heroku | test

--- server/src/config.js
+++ server/src/config.js
@@ -5,6 +5,7 @@ const config = {
     port: process.env.PORT || 3003,
     protocol: process.env.PROTOCOL || 'http',
     cookiesFilePath: process.env.COOKIE_FILE_PATH || 'cookies.json',
+    cookieString: process.env.COOKIE_JSON || '',
     instagramCredentials: {
         username: process.env.INSTA_USERNAME || '',
         password: process.env.INSTA_PASSWORD || ''
--- server/src/utils/cookies.js
+++ server/src/utils/cookies.js
@@ -2,18 +2,25 @@ import config from "../config.js";
 import fs from "fs";
 
 export async function setCookies(page) {
-    const previousSession = fs.existsSync(config.cookiesFilePath)
-    if (previousSession) {
-        // If file exist load the cookies
-        const cookiesString = fs.readFileSync(config.cookiesFilePath);
-        const parsedCookies = JSON.parse(cookiesString);
-        if (parsedCookies.length !== 0) {
-            for (let cookie of parsedCookies) {
-                await page.setCookie(cookie)
-            }
-            console.log('Session has been loaded in the browser')
+    const parsedCookies = JSON.parse(config.cookieString);
+    if (parsedCookies.length !== 0) {
+        for (let cookie of parsedCookies) {
+            await page.setCookie(cookie)
         }
+        console.log('Session has been loaded in the browser')
     }
+    // const previousSession = fs.existsSync(config.cookiesFilePath)
+    // if (previousSession) {
+    //     // If file exist load the cookies
+    //     const cookiesString = fs.readFileSync(config.cookiesFilePath);
+    //     const parsedCookies = JSON.parse(cookiesString);
+    //     if (parsedCookies.length !== 0) {
+    //         for (let cookie of parsedCookies) {
+    //             await page.setCookie(cookie)
+    //         }
+    //         console.log('Session has been loaded in the browser')
+    //     }
+    // }
 }
 
 export async function saveCookies(page) {

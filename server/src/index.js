a527bf53d3f2f3d9b1140f057bb400ddf280ff15
jannkasper <jann.kasper@gmail.com>
2021-02-18 05:51:45 +0100

    heroku | fix load more

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import http from "http";
 import https from "https";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
@@ -10,18 +9,17 @@ import setRoutes from "./routes.js"
 // import { startSession } from "./utils/session.js";
 
 const app = express();
-app.use(cors());
 app.use(morgan('dev'));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 setRoutes(app);
 
 // startSession("https://www.instagram.com/accounts/login/");
 
-const protocol = config.protocol === 'http' ? http : https;
-const server = protocol.createServer(app);
+// const protocol = config.protocol === 'http' ? http : https;
+const server = http.createServer(app);
 
 const init = async () => {
     // await startSession("https://www.instagram.com/accounts/login/");

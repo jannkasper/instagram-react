4682c95347cdde6c594a0263bc6889802b979e0c
jannkasper <jann.kasper@gmail.com>
2021-02-20 11:05:10 +0100

    heroku | fix load more

--- server/src/controllers/posts.js
+++ server/src/controllers/posts.js
@@ -1,6 +1,24 @@
 import {getPage, startBrowser} from "../utils/session.js";
 import axios from "axios";
 
+const config = {
+    headers: {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'pl,en-US;q=0.9,en;q=0.8,nl;q=0.7,nb;q=0.6',
+        'cache-control': 'max-age=0',
+        'cookie': 'ig_cb=2; ig_did=88BB498E-E955-405F-A830-3DB7BC47A8AE; mid=X-DhrwAEAAFWBn9a9lE9zRScjQRf; fbm_124024574287414=base_domain=.instagram.com; shbid=19194; datr=Ay8iYAsdTFJuw4kRnu92mOox; rur=FRC; ig_lang=en; shbts=1613798901.7801588; csrftoken=nC97Ey2iGugRvpXV6IgJObMioTqMfbXR; ds_user_id=46117109912; sessionid=46117109912%3AJ7Tjs5nF5w4nqU%3A12; fbsr_124024574287414=FDYq4ILYfS3S4JrV0WFmVKpTyl_2nZiHOqWnoZcBNIU.eyJ1c2VyX2lkIjoiMTAwMDAxMTMyMjcxMzU5IiwiY29kZSI6IkFRRHVCSVlXelBuSjJlT0RXZFFTWnpxY09Db0YtaVoyOC1nY0M2ZElqUkVxMHNVTi1wN0t2T1JGLXNSX3VKVlRhd0F2eFRMS1JzeWhkZ3FHaFU5UUZXcW9ydXQxTzEwVy0xLVZIbFI3dFQtM2x3QUlKZ2dpalJ5TkJiQTkyRnMyRzJWV1FKSDFvS0l1RVBsalMzXzBad2F6ckE5MVgyZnVRdmwxZkdMM0dGSWdlTUU3clVGd25QQ002VFdXLTZ3djJ3bGZBaGFvaS1qOXkxaVVSOVFKeklqb1BMZzVVbllhMEhCc2pZMUpzWGlvWkpCajdvYlc1RGlfSS1fZ2p0VTJkS0s2ZFVCTmdNcElEVUU0ZW9PTEVJSzFPNTNFZ1JZVnFEakdOQmNSODBLWDdreEd6MlplSVRsNk5EUG92akxMZk1sTVpDQjI4ZDE1b1oxMDg1VC1uNXF2Iiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQU9ZNERIV3pEbWpyYXpDWkJhWHVwMkRBZmx0c1pDMDhBcjV4amtzRGVnZEhKUkFSNlZjNlpBT1pDWkFiUDRQT2xvbVlpSExVODZoUzhGSm81U09ORlNvRjEydFpDcmJBVnI2TlRhR2tpUEtuTnk4dlVFQTFaQmJkbUhXWXdaQTFwSzl0ejBVd3dPVDhyV2ptQlpBZFNaQXBLWEJDRk5OTkNIQUtaQWc1dGVCZmd1ZCIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjoxNjEzODE1MjQxfQ',
+        'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    }
+}
+
 export const postContent = async (req, res) => {
     const postId = req.params.postId;
     // const page = getPage();
@@ -18,7 +36,7 @@ export const postContent = async (req, res) => {
     //     return window.__additionalData
     // });
 
-    const sharedData = await axios.get(`https://www.instagram.com/p/${postId}`)
+    const sharedData = await axios.get(`https://www.instagram.com/p/${postId}`, config)
         .then(function (response) {
             // handle success
             const jsonObject = response.data.match(/<script type="text\/javascript">window\._sharedData = (.*)<\/script>/)[1].slice(0, -1)
@@ -164,7 +182,7 @@ export const morePostsContent = async (req, res) => {
         .replace(':', '%3A')
         .replace('"', '%22')
         .replace('=', '%3D');
-    const data = await axios.get(url + transformParams)
+    const data = await axios.get(url + transformParams, config)
         .then(function (response) {
             // handle success
             return response.data;
